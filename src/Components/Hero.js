86018bd29cb90a7b44fe34232b60db369b6a04cb
DevTaylor <97462163+ziplabdev@users.noreply.github.com>
2022-10-01 10:22:00 -0700

    updated favicon.ico

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './hero.css'
+import BackgroundPhoto from '../assets/main-background.png'
 import FacePhoto from '../assets/face-photo.png'
 export default function Hero(props) {
 
     return (
-        <div className="hero-container">
-            <div className="content-container">
+        <div className="hero-container" style={{ backgroundImage: {BackgroundPhoto}}}>
+            <div className="content-container" style={{ backgroundImage: {BackgroundPhoto}}}>
                 <div className="hero-text">
                     <div className="t-80-bold">Rainier <br/>Dirawatun</div>
                     <div className="t-30">I'm a Software Engineer</div>
